3d5244170d13b9c36b2136652d6744c00192db02
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-07-07 22:37:21 -0300

    projeto ok

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # churrascometro
+
+Projeto que calcula a quantidade de carne e bebidas com relação a quantidade de pessoas e tempo para um churrasco
---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+let inputAdultos = document.querySelector('#adultos')
+let inputCriancas = document.querySelector('#criancas')
+let inputDuracao = document.querySelector('#duracao')
+
+let resultado = document.querySelector('#resultado')
+
+function calcular() {
+  console.log('calculando...')
+
+  let adultos = inputAdultos.value
+  let criancas = inputCriancas.value
+  let duracao = inputDuracao.value
+
+  let qtdTotalCarne =
+    carnePP(duracao) * adultos + (carnePP(duracao) / 2) * criancas
+
+  let qtdTotalCerveja = cervejaPP(duracao) * adultos
+
+  let qtdTotalBebida =
+    bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2) * criancas
+
+  resultado.innerHTML = `<p>${qtdTotalCarne / 1000} Kg de Carne</p>`
+  resultado.innerHTML += `<p>${Math.ceil(
+    qtdTotalCerveja / 350
+  )} latas de Cerveja</p>`
+  resultado.innerHTML += `<p>${Math.ceil(
+    qtdTotalBebida / 2000
+  )} Pet's 2l de Bebidas</p>`
+}
+
+function carnePP(duracao) {
+  if (duracao >= 6) {
+    return 600
+  } else {
+    return 350
+  }
+}
+
+function cervejaPP(duracao) {
+  if (duracao >= 6) {
+    return 2000
+  } else {
+    return 1200
+  }
+}
+
+function bebidasPP(duracao) {
+  if (duracao >= 6) {
+    return 1000
+  } else {
+    return 500
+  }
+}
